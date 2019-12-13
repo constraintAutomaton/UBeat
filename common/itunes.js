262318471696109ba9abf765652170aab2063770
Bryan <tambryanelliott@gmail.com>
2019-12-13 17:45:07 -0500

    change in highres image AQ for single result

--- common/itunes.js
+++ common/itunes.js
@@ -8,57 +8,56 @@ exports.search = function(parameters, res) {
   queryItunesApi(searchEndPoint + qs.stringify(parameters), res)
 }
 
-exports.lookup = function(parameters, res, amount,send=true,next=false, isAlbumArtist=false) {
-  queryItunesApi(lookupEndPoint + qs.stringify(parameters), res, amount,send,next,isAlbumArtist)
+exports.lookup = function(
+  parameters,
+  res,
+  amount,
+  send = true,
+  next = false,
+  isAlbumArtist = false
+) {
+  queryItunesApi(lookupEndPoint + qs.stringify(parameters), res, amount, send, next, isAlbumArtist)
 }
-async function queryItunesApi(url, res, amount,send=true,next=false, isAlbumArtist=false) {
+async function queryItunesApi(url, res, amount, send = true, next = false, isAlbumArtist = false) {
   try {
     const { data } = await axios.get(url)
 
     if (amount == 'many') {
       data.results.splice(0, 1)
-      data.results.map(el => {
-        if (res.locals.highResImage != undefined) {
-          el.highResImage = res.locals.highResImage
-        }
+      if (res.locals.highResImage != undefined) {
+        data.results[0].highResImage = res.locals.highResImage
+      }
+      if (res.locals.bio != undefined) {
+        data.results[0].highResImage = res.locals.bio
+      }
 
-        if (res.locals.bio != undefined) {
-          el.bio = res.locals.bio
-        }
-      })
       data.resultCount--
-      if(send===true){
+      if (send === true) {
         res.status(200).send(data)
-      }else{
-        res.locals.data=data
+      } else {
+        res.locals.data = data
         res.locals.send = false
-        if(isAlbumArtist===true){
-          res.locals.isAlbumArtist = true
-        }
-        
-        next()
-      }
-      
-    } else {
-      data.results.map(el => {
-        if (res.locals.highResImage != undefined) {
-          el.highResImage = res.locals.highResImage
-        }
 
-        if (res.locals.bio != undefined) {
-          el.bio = res.locals.bio
-        }
-      })
-      if(send===true){
-        res.status(200).send(data)
-      }else{
-        res.locals.data=data
-        
-        res.locals.send = false
-        
         next()
       }
-      
+    } else {
+      if (res.locals.highResImage != undefined) {
+        data.results[0].highResImage = res.locals.highResImage
+      }
+
+      if (res.locals.bio != undefined) {
+        data.results[0].bio = res.locals.bio
+      }
+
+      if (send === true) {
+        res.status(200).send(data)
+      } else {
+        res.locals.data = data
+
+        res.locals.send = false
+
+        next()
+      }
     }
   } catch (err) {
     err && err.response && res.status(err.response.status).send(err.response.data)
--- routes/status.js
+++ routes/status.js
@@ -1,5 +1,6 @@
 exports.getHome = function(req, res) {
-  res.status(200).send('Welcome to UBeat API.')
+  res.set('Content-Type', 'text/html');
+  res.status(200).send('<h1>Welcome to the UBeat custom API.</h1><p>Provided by team 9</p>');
 }
 
 exports.getStatus = function(req, res) {
