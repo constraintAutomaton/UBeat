257fe63b52173dceea081f7232e481d9d1ea5eea
Bryan <tambryanelliott@gmail.com>
2019-12-14 11:52:12 -0500

    change max image search

--- routes/discogs.js
+++ routes/discogs.js
@@ -57,7 +57,7 @@ const search = async (req, res, mode) => {
   try {
     const resData = { results: [] }
     for (i in req.body.query) {
-      if (String(i) === '5') {
+      if (String(i) === '8') {
         break
       }
       const el = req.body.query[i]
