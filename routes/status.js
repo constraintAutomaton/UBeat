e8406916b212be6c3c8ec589ff7f100d8f259416
Bryan <tambryanelliott@gmail.com>
2019-12-15 19:52:06 -0500

    changed of the main route message

--- routes/status.js
+++ routes/status.js
@@ -1,6 +1,6 @@
 exports.getHome = function(req, res) {
   res.set('Content-Type', 'text/html');
-  res.status(200).send('<h1>Welcome to the UBeat custom API.</h1><p>Provided by team 9</p>');
+  res.status(200).send('<h1>Welcome to the UBeat custom API.</h1><p>Provided by team 7</p>');
 }
 
 exports.getStatus = function(req, res) {
